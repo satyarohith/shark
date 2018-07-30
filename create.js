e9e8ba504801a3d63d67a90f147586bd2f782a23
Satya Rohith <me@satyarohith.com>
2018-07-30 19:24:22 +0530

    domain Creact, Delete, List initial stage

--- create.js
+++ create.js
@@ -24,6 +24,17 @@ module.exports = {
 
     return inquirer.prompt(questions);
   },
+  domain: () => {
+    const questions = [
+      {
+        type: 'input',
+        name: 'domain_name',
+        message: 'Enter your domain name:',
+        filter: input => input.toLowerCase()
+      }
+    ];
+    return inquirer.prompt(questions);
+  },
   droplet: () => {
     const questions = [
       {
