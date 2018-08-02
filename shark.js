cca2b8f71d5737383617866a270779244c27197a
Satya Rohith <satyarohith@gmx.com>
2018-08-02 22:34:43 +0530

    use callMatchingMethod

--- shark.js
+++ shark.js
@@ -3,72 +3,18 @@ const Create = require('./actions/create');
 const Delete = require('./actions/delete');
 const List = require('./actions/list');
 const { Init } = require('./actions/init');
+const { callMatchingMethod } = require('./util');
 
 module.exports = () => {
   switch (argv._[0]) {
     case 'create':
-      {
-        switch (argv._[1]) {
-          case 'droplet':
-            Create.droplet();
-            break;
-          case 'domain':
-            Create.domain();
-            break;
-          case 'ssh_key':
-          case 'sshkey':
-            Create.sshkey();
-            break;
-          default:
-            Create.init();
-            break;
-        }
-      }
+      callMatchingMethod(Create, argv._[1]);
       break;
     case 'delete':
-      {
-        switch (argv._[1]) {
-          case 'droplet':
-            Delete.droplet();
-            break;
-          case 'token':
-            Delete.token();
-            break;
-          case 'domain':
-            Delete.domain();
-            break;
-          case 'ssh_key':
-          case 'sshkey':
-            Delete.sshkey();
-            break;
-          default:
-            Delete.init();
-            break;
-        }
-      }
+      callMatchingMethod(Delete, argv._[1]);
       break;
     case 'list':
-      {
-        switch (argv._[1]) {
-          case 'droplet':
-          case 'droplets':
-            List.droplets();
-            break;
-          case 'domain':
-          case 'domains':
-            List.domains();
-            break;
-          case 'sshkeys':
-          case 'ssh_key':
-          case 'ssh_keys':
-          case 'sshkey':
-            List.sshKeys();
-            break;
-          default:
-            List.init();
-            break;
-        }
-      }
+      callMatchingMethod(List, argv._[1]);
       break;
     default:
       Init();
