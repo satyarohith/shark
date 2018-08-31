6c087eecf44740d2675b1bb6d5f0822d5fa196a7
Satya Rohith <satyarohith@gmx.com>
2018-08-31 09:27:43 +0530

    fix: prompt global install when notifying about update

--- shark.js
+++ shark.js
@@ -10,7 +10,7 @@ const updateNotifier = require('update-notifier');
 const pkg = require('./package.json');
 const notifier = updateNotifier({ pkg });
 
-notifier.notify();
+notifier.notify({ isGlobal: true });
 
 initAccount();
 
