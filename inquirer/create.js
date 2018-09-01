c980e8149c66ed0d8660fae0d4b6588c123ac079
Satya Rohith <me@satyarohith.com>
2018-09-01 20:59:22 +0530

    style(inquirer): change export style

--- inquirer/create.js
+++ inquirer/create.js
@@ -8,171 +8,176 @@ const {
   loadAvailableSSHKEYS
 } = require('../loaders');
 
-module.exports = {
-  init: () => {
-    const questions = [
-      {
-        type: 'list',
-        name: 'create',
-        message: 'What do you want to create?',
-        choices: [
-          { name: 'Droplet', value: 'droplet' },
-          { name: 'Floating Ip', value: 'floating_ip' },
-          { name: 'Domain', value: 'domain' },
-          { name: 'SSH Key', value: 'ssh_key' },
-          { name: 'Volume', value: 'volume' },
-          { name: '<- Back', value: 'back' },
-          { name: 'Exit', value: 'exit' }
-        ]
-      }
-    ];
+module.exports.init = () => {
+  const questions = [
+    {
+      type: 'list',
+      name: 'create',
+      message: 'What do you want to create?',
+      choices: [
+        { name: 'Droplet', value: 'droplet' },
+        { name: 'Floating Ip', value: 'floating_ip' },
+        { name: 'Domain', value: 'domain' },
+        { name: 'SSH Key', value: 'ssh_key' },
+        { name: 'Volume', value: 'volume' },
+        { name: '<- Back', value: 'back' },
+        { name: 'Exit', value: 'exit' }
+      ]
+    }
+  ];
 
-    return inquirer.prompt(questions);
-  },
-  accessToken: () => {
-    const questions = [
-      {
-        type: 'input',
-        message: chalk.yellow('Please provide your access token:'),
-        name: 'do_api_access_token'
-      }
-    ];
+  return inquirer.prompt(questions);
+};
 
-    return inquirer.prompt(questions);
-  },
-  domain: () => {
-    const questions = [
-      {
-        type: 'input',
-        name: 'domain_name',
-        message: 'Enter your domain name:',
-        filter: input => input.toLowerCase()
-      }
-    ];
-    return inquirer.prompt(questions);
-  },
-  droplet: async (DoAPI, spinner) => {
-    const questions = [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'What should we call your Droplet?'
-      },
-      {
-        type: 'list',
-        name: 'image',
-        message: 'Which Distribution do you want to use?',
-        choices: await loadAvailableImages(DoAPI, spinner)
-      },
-      {
-        type: 'list',
-        name: 'size',
-        message: 'How stronger your computer should be?',
-        choices: await loadAvailableSizes(DoAPI, spinner),
-        pageSize: 2
-      },
-      {
-        type: 'list',
-        name: 'region',
-        message: 'Choose your data center!',
-        choices: await loadAvailableRegions(DoAPI, spinner)
-      },
-      {
-        type: 'checkbox',
-        name: 'dropletAddOps',
-        message: 'Add any additional options',
-        choices: [
-          new inquirer.Separator(' == Additional Options == '),
-          {
-            name: 'Private networking',
-            value: 'private_networking'
-          },
-          {
-            name: 'IPv6',
-            value: 'ipv6'
-          },
-          {
-            name: 'Monitoring',
-            value: 'monitoring'
-          }
-        ]
-      },
-      {
-        type: 'input',
-        name: 'tags',
-        message: 'Add any droplet tags',
-        filter: input => {
-          let tags = input.split(' ');
-          return tags;
+module.exports.accessToken = () => {
+  const questions = [
+    {
+      type: 'input',
+      message: chalk.yellow('Please provide your access token:'),
+      name: 'do_api_access_token'
+    }
+  ];
+
+  return inquirer.prompt(questions);
+};
+
+module.exports.droplet = async () => {
+  const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What should we call your Droplet?'
+    },
+    {
+      type: 'list',
+      name: 'image',
+      message: 'Which Distribution do you want to use?',
+      choices: await loadAvailableImages()
+    },
+    {
+      type: 'list',
+      name: 'size',
+      message: 'How stronger your computer should be?',
+      choices: await loadAvailableSizes(),
+      pageSize: 2
+    },
+    {
+      type: 'list',
+      name: 'region',
+      message: 'Choose your data center!',
+      choices: await loadAvailableRegions()
+    },
+    {
+      type: 'checkbox',
+      name: 'dropletAddOps',
+      message: 'Add any additional options',
+      choices: [
+        new inquirer.Separator(' == Additional Options == '),
+        {
+          name: 'Private networking',
+          value: 'private_networking'
+        },
+        {
+          name: 'IPv6',
+          value: 'ipv6'
+        },
+        {
+          name: 'Monitoring',
+          value: 'monitoring'
         }
-      },
-      {
-        type: 'checkbox',
-        name: 'ssh_keys',
-        message: 'Select your SSH keys',
-        choices: await loadAvailableSSHKEYS(DoAPI, spinner)
+      ]
+    },
+    {
+      type: 'input',
+      name: 'tags',
+      message: 'Add any droplet tags',
+      filter: input => {
+        let tags = input.split(' ');
+        return tags;
       }
-    ];
+    },
+    {
+      type: 'checkbox',
+      name: 'ssh_keys',
+      message: 'Select your SSH keys',
+      choices: await loadAvailableSSHKEYS()
+    }
+  ];
 
-    return inquirer.prompt(questions);
-  },
-  ssh_key: () => {
-    const questions = [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Give a name to your key:'
-      },
-      {
-        type: 'input',
-        name: 'public_key',
-        message: 'Paste your public key:'
+  return inquirer.prompt(questions);
+};
+
+module.exports.domain = () => {
+  const questions = [
+    {
+      type: 'input',
+      name: 'domain_name',
+      message: 'Enter your domain name:',
+      filter: input => input.toLowerCase()
+    }
+  ];
+
+  return inquirer.prompt(questions);
+};
+
+module.exports.ssh_key = () => {
+  const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Give a name to your key:'
+    },
+    {
+      type: 'input',
+      name: 'public_key',
+      message: 'Paste your public key:'
+    }
+  ];
+
+  return inquirer.prompt(questions);
+};
+
+module.exports.floating_ip = async () => {
+  const questions = [
+    {
+      type: 'list',
+      name: 'region_slug',
+      message: 'Select the region where floating_ip has to be reserved',
+      choices: await loadAvailableRegions()
+    }
+  ];
+
+  return inquirer.prompt(questions);
+};
+
+module.exports.volume = async () => {
+  const questions = [
+    {
+      type: 'input',
+      name: 'volume_name',
+      message: 'Input your Volume Name'
+    },
+    {
+      type: 'input',
+      name: 'volume_size',
+      message: 'Enter your volume size in GB',
+      filter: input => {
+        let size = parseInt(input, 10);
+        return size;
       }
-    ];
+    },
+    {
+      type: 'input',
+      name: 'volume_desc',
+      message: 'Any description about your volume?'
+    },
+    {
+      type: 'list',
+      name: 'volume_region',
+      message: 'Select volume region',
+      choices: await loadAvailableRegions()
+    }
+  ];
 
-    return inquirer.prompt(questions);
-  },
-  floating_ip: async () => {
-    const questions = [
-      {
-        type: 'list',
-        name: 'region_slug',
-        message: 'Select the region where floating_ip has to be reserved',
-        choices: await loadAvailableRegions()
-      }
-    ];
-
-    return inquirer.prompt(questions);
-  },
-  volume: async (DoAPI, spinner) => {
-    const questions = [
-      {
-        type: 'input',
-        name: 'volume_name',
-        message: 'Input your Volume Name'
-      },
-      {
-        type: 'input',
-        name: 'volume_size',
-        message: 'Enter your volume size in GB',
-        filter: input => {
-          let size = parseInt(input, 10);
-          return size;
-        }
-      },
-      {
-        type: 'input',
-        name: 'volume_desc',
-        message: 'Any description about your volume?'
-      },
-      {
-        type: 'list',
-        name: 'volume_region',
-        message: 'Select volume region',
-        choices: await loadAvailableRegions(DoAPI, spinner)
-      }
-    ];
-
-    return inquirer.prompt(questions);
-  }
+  return inquirer.prompt(questions);
 };
--- inquirer/delete.js
+++ inquirer/delete.js
@@ -9,86 +9,89 @@ const {
   loadAvailableVolumes
 } = require('../loaders');
 
-module.exports = {
-  init: () => {
-    const questions = [
-      {
-        type: 'list',
-        name: 'delete',
-        message: 'What do you want to Delete?',
-        choices: [
-          { name: 'Droplet', value: 'droplet' },
-          { name: 'Floating Ip', value: 'floating_ip' },
-          { name: 'Domain', value: 'domain' },
-          { name: 'SSH Key', value: 'ssh_key' },
-          { name: 'Volume', value: 'volume' },
-          { name: '<- Back', value: 'back' },
-          { name: 'Exit', value: 'exit' }
-        ]
-      }
-    ];
+module.exports.init = () => {
+  const questions = [
+    {
+      type: 'list',
+      name: 'delete',
+      message: 'What do you want to Delete?',
+      choices: [
+        { name: 'Droplet', value: 'droplet' },
+        { name: 'Floating Ip', value: 'floating_ip' },
+        { name: 'Domain', value: 'domain' },
+        { name: 'SSH Key', value: 'ssh_key' },
+        { name: 'Volume', value: 'volume' },
+        { name: '<- Back', value: 'back' },
+        { name: 'Exit', value: 'exit' }
+      ]
+    }
+  ];
 
-    return inquirer.prompt(questions);
-  },
-  domain: async (DoAPI, spinner) => {
-    const questions = [
-      {
-        type: 'checkbox',
-        name: 'domain_name',
-        message: chalk.red('Select the domain you want to delete:'),
-        choices: await loadAvailableDomains(DoAPI, spinner)
-      }
-    ];
+  return inquirer.prompt(questions);
+};
 
-    return inquirer.prompt(questions);
-  },
-  droplet: async () => {
-    const questions = [
-      {
-        type: 'checkbox',
-        name: 'droplets',
-        message: 'Select the droplet you want to delete:',
-        choices: await loadAvailableDroplets()
-        // TODO: handle if no droplet is selected
-      }
-    ];
+module.exports.domain = async () => {
+  const questions = [
+    {
+      type: 'checkbox',
+      name: 'domain_name',
+      message: chalk.red('Select the domain you want to delete:'),
+      choices: await loadAvailableDomains()
+    }
+  ];
 
-    return inquirer.prompt(questions);
-  },
-  ssh_key: async (DoAPI, spinner) => {
-    const questions = [
-      {
-        type: 'checkbox',
-        name: 'ssh_key_id',
-        message: 'Select ssh_key you want to delete:',
-        choices: await loadAvailableSSHKEYS(DoAPI, spinner)
-      }
-    ];
+  return inquirer.prompt(questions);
+};
 
-    return inquirer.prompt(questions);
-  },
-  floating_ip: async () => {
-    const questions = [
-      {
-        type: 'checkbox',
-        name: 'floating_ip',
-        message: 'Select floating_ips you want to delete:',
-        choices: await loadAvailableFloatingIps()
-      }
-    ];
+module.exports.droplet = async () => {
+  const questions = [
+    {
+      type: 'checkbox',
+      name: 'droplets',
+      message: 'Select the droplet you want to delete:',
+      choices: await loadAvailableDroplets()
+      // TODO: handle if no droplet is selected
+    }
+  ];
 
-    return inquirer.prompt(questions);
-  },
-  volume: async (DoAPI, spinner) => {
-    const questions = [
-      {
-        type: 'checkbox',
-        name: 'volumes',
-        message: 'Select volumes you want to delete:',
-        choices: await loadAvailableVolumes(DoAPI, spinner)
-      }
-    ];
+  return inquirer.prompt(questions);
+};
 
-    return inquirer.prompt(questions);
-  }
+module.exports.ssh_key = async () => {
+  const questions = [
+    {
+      type: 'checkbox',
+      name: 'ssh_key_id',
+      message: 'Select ssh_key you want to delete:',
+      choices: await loadAvailableSSHKEYS(DoAPI, spinner)
+    }
+  ];
+
+  return inquirer.prompt(questions);
+};
+
+module.exports.floating_ip = async () => {
+  const questions = [
+    {
+      type: 'checkbox',
+      name: 'floating_ip',
+      message: 'Select floating_ips you want to delete:',
+      choices: await loadAvailableFloatingIps()
+    }
+  ];
+
+  return inquirer.prompt(questions);
+};
+
+module.exports.volume = async () => {
+  const questions = [
+    {
+      type: 'checkbox',
+      name: 'volumes',
+      message: 'Select volumes you want to delete:',
+      choices: await loadAvailableVolumes()
+    }
+  ];
+
+  return inquirer.prompt(questions);
 };
