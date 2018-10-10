48a54cfd254f3d57bcb78b6782fd0f2364d364af
Satya Rohith <gsatyarohith@gmail.com>
2018-10-10 14:25:26 +0530

    test: change test to check travis

--- shark.test.js
+++ shark.test.js
@@ -4,10 +4,10 @@ const pkg = require('./package.json');
 jest.setTimeout(10000);
 
 describe('shark', () => {
-  it('shows version with flag --version', async done => {
+  it('outputs version with flag --version', async () => {
+    expect.assertions(1);
     const { stdout } = await execa('./shark.js', ['--version']);
     const expected = pkg.version;
     expect(stdout).toBe(expected);
-    done();
   });
 });
