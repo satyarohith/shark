eab24c5fac9802166c10d2970f7ba42e7da225a5
Satya Rohith <gsatyarohith@gmail.com>
2018-10-10 15:16:23 +0530

    test: use ava and test travis

--- shark.test.js
+++ shark.test.js
@@ -1,13 +1,9 @@
+const test = require('ava');
 const execa = require('execa');
 const pkg = require('./package.json');
 
-jest.setTimeout(10000);
-
-describe('shark', () => {
-  it('outputs version with flag --version', async () => {
-    expect.assertions(1);
-    const { stdout } = await execa('./shark.js', ['--version']);
-    const expected = pkg.version;
-    expect(stdout).toBe(expected);
-  });
+test('outputs version with flag --version', async t => {
+  const { stdout } = await execa('./shark.js', ['--version']);
+  const expected = pkg.version;
+  t.is(stdout, expected);
 });
