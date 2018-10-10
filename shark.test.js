defb0e9e842918dc8a6ddaa3bd548d4db20ac320
Satya Rohith <gsatyarohith@gmail.com>
2018-10-10 14:17:40 +0530

    test: change jest timeout to support travis

--- shark.test.js
+++ shark.test.js
@@ -1,6 +1,8 @@
 const execa = require('execa');
 const pkg = require('./package.json');
 
+jest.setTimeout(10000);
+
 describe('shark', () => {
   it('shows version with flag --version', async done => {
     const { stdout } = await execa('./shark.js', ['--version']);
