6ef425f5d1feb461cf3a157f8a6966c0466e65a4
Satya Rohith <gsatyarohith@gmail.com>
2018-12-23 15:54:18 +0530

    refactor(domains): update prompts

--- src/prompts/index.js
+++ src/prompts/index.js
@@ -1,5 +1,8 @@
 const {prompt} = require('enquirer');
 
+// Check https://github.com/enquirer/enquirer/issues/13#issuecomment-447602031
+prompt.on('cancel', () => process.exit());
+
 const askToken = () => {
   return prompt({
     type: 'input',
@@ -8,6 +11,31 @@ const askToken = () => {
   });
 };
 
+const askDomainName = () => {
+  return prompt({
+    type: 'input',
+    name: 'domainName',
+    message: 'Enter your domain name:'
+  });
+};
+
+const domainsInit = () => {
+  return prompt({
+    type: 'select',
+    name: 'command',
+    message: 'What to do?',
+    initial: 2,
+    choices: [
+      {name: 'create', message: 'Create domains'},
+      {name: 'delete', message: 'Delete domains'},
+      {name: 'list', message: 'List domains'},
+      {name: 'exit', message: 'Exit'}
+    ]
+  });
+};
+
 module.exports = {
-  askToken
+  askToken,
+  domainsInit,
+  askDomainName
 };
