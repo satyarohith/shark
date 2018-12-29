08212d26d2cf14db988f7939849d7b8193ce2bd5
Satya Rohith <me@satyarohith.com>
2018-12-29 14:22:54 +0530

    docs(ssh_keys:get): fix typo in help

--- src/commands/ssh_keys/get.js
+++ src/commands/ssh_keys/get.js
@@ -41,7 +41,10 @@ SSHKeysGetCommand.description = 'get public_key by id/fingerprint';
 SSHKeysGetCommand.flags = {
   json: flags.boolean({char: 'j', description: 'output in json format'}),
   id: flags.string({char: 'i', description: 'get ssh_key by id'}),
-  fingerprint: flags.string({char: 'f', description: 'get ssh_key by id'})
+  fingerprint: flags.string({
+    char: 'f',
+    description: 'get ssh_key by fingerprint'
+  })
 };
 
 module.exports = SSHKeysGetCommand;
