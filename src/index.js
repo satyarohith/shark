fc1587a9b23d01c52c6e6a2a6d84fc605449101d
Satya Rohith <gsatyarohith@gmail.com>
2019-01-27 10:35:44 +0530

    refactor: add update notifier

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+
+try {
+  updateNotifier({
+    pkg
+  }).notify({isGlobal: true});
+} catch (error) {
+  console.error('Error checking for updates:', error.message);
+}
+
 module.exports = require('@oclif/command');
